c5a68b6d365271ebbd69a4c13248d94e6a068231
LLihao <h_91@me.com>
2017-01-24 14:52:29 -0800

    improve 1.4 solution, and better bit manipulation efficiency

--- src/QS01Arrays.java
+++ src/QS01Arrays.java
@@ -95,7 +95,6 @@ public class QS01Arrays {
             }
         }
         if (spaceCount == 0) { return; }
-
         int end = trueLength + spaceCount * 2;
         input[end--] = '\0';
         int index = trueLength - 1;
@@ -109,7 +108,6 @@ public class QS01Arrays {
                 input[index--] = input[end--];
             }
         }
-
     }
 
 
@@ -137,6 +135,43 @@ public class QS01Arrays {
             }
         }
         return true;
+    }
+
+    boolean isPermutationOfPalindromeLetterOnly(String word) {
+        int table = 0;
+        for (char c : word.toCharArray()) {
+            int mask = 1 << (c - 'a');
+            if ((mask & table) != 0) { // if this bit is on
+                table &= ~mask; // set bit
+            } else {
+                table |= mask; // clear bit
+            }
+        }
+        return checkEachBits(table);
+    }
+
+    // Helper bit manipulation functions
+    // not efficient
+    private boolean checkEachBits(int table) {
+        boolean oddChar = false;
+        for (int i = 0; i < 26; ++i) {
+            int mask = 1 << i;
+            if ((table & mask) > 0) {
+                if (oddChar) {
+                    return false;
+                }
+                oddChar = true;
+            }
+        }
+        return true;
+    }
+
+    // more efficent
+    // 00010000 - 1 = 00001111
+    // 00010000 & 00001111 = 0
+    private boolean checkBits(int table) {
+        int val = table - 1;
+        return (table & val) == 0;
     }
 
     /**
