3f852723be5afb4b6ccc230caca42118d4f7b492
LLihao <h_91@me.com>
2017-01-24 22:33:53 -0800

    fix possible crash for 1.9

--- src/QS01Arrays.java
+++ src/QS01Arrays.java
@@ -320,6 +320,7 @@ public class QS01Arrays {
      *
      */
     boolean isSubstring(String s1, String s2) {
+        if (s1.length() < 1 || s1.length() != s2.length()) { return false; }
         String s1s1 = s1 + s1;
         return s1s1.contains(s2);
     }
