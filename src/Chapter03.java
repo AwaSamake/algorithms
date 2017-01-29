4d06f467216c90f8413e3d13db7afe66fc44289a
LLihao <h_91@me.com>
2017-01-28 22:39:44 -0800

    completed 3.5 SortStack

--- src/Chapter03.java
+++ src/Chapter03.java
@@ -220,6 +220,25 @@ class MyQueue <T> {
  *
  */
 
+class SortStack {
+
+    public void sort(Stack<Integer> s1) {
+        Stack<Integer> s2 = new Stack<Integer>();
+
+        while (!s1.isEmpty()) {
+            int temp = s1.pop();
+            while (!s2.isEmpty() && s2.peek() > temp) {
+                s1.push(s2.pop());
+            }
+            s2.push(temp);
+        }
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+}
+
 
 /**
  *
