1767528f22fc11ab0eb7bd09876ba5bba2b279d1
LLihao <h_91@me.com>
2017-01-28 17:43:47 -0800

    completed 3.4 MyQueue

--- src/Chapter03.java
+++ src/Chapter03.java
@@ -176,6 +176,41 @@ class SetOfStacks {
  *
  */
 
+class MyQueue <T> {
+    private Stack<T> out;
+    private Stack<T> in;
+
+    public MyQueue() {
+        out = new Stack<T>();
+        in = new Stack<T>();
+    }
+
+    public int size() {
+        return in.size() + out.size();
+    }
+
+    public void enqueue(T val) {
+        in.push(val);
+    }
+
+    public T dequeue() {
+        shift();
+        return out.pop();
+    }
+
+    public T peek() {
+        shift();
+        return out.peek();
+    }
+
+    private void shift() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+    }
+}
 
 /**
  *
