901a9ac164b72085d89a0eebea4d1e96a49c9484
LLihao <h_91@me.com>
2017-01-30 20:35:39 -0800

    5.2 Book Solution

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -57,6 +57,32 @@ class Insertion {
  *
  */
 
+class BinaryToString {
+    public String printBinary(double num) {
+        if (num < 0 || num > 1) {
+            return "ERROR";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        binary.append('.');
+        while (num > 0) {
+            if (binary.length() >= 32) {
+                return "ERROR";
+            }
+             num *= 2;
+            if (num > 1) {
+                binary.append('1');
+                num -= 1;
+            } else {
+                binary.append('0');
+            }
+        }
+        return binary.toString();
+    }
+}
+
+
+
 
 /**
  *
