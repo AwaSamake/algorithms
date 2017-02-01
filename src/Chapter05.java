02e3ffe9fffa2da9919a73f00dd297844fa28bb7
LLihao <h_91@me.com>
2017-01-31 22:14:55 -0800

    5.5 Three Solutions
    Solution #1: Lihao
    Solution #2: Book
    Solution #3: Book

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -212,6 +212,35 @@ class NextNumber {
  *
  */
 
+class Conversion {
+    public int convert(int A, int B) {
+        int diff = A ^ B;
+        int count = 0;
+        while (diff != 0) {
+            if ((diff & 1) == 1) {
+                ++count;
+            }
+            diff >>= 1;
+        }
+        return count;
+    }
+
+    public int convert2(int A, int B) {
+        int count = 0;
+        for (int c = A ^ B; c != 0; c >>= 1) {
+            count += (c & 1);
+        }
+        return count;
+    }
+
+    public int convert3(int A, int B) {
+        int count = 0;
+        for (int c = A ^ B; c != 0; c &= (c-1)) {
+            ++count;
+        }
+        return count;
+    }
+}
 
 /**
  *
