ff8edd0f7f1b1bb6ebd9ba466186c6efc074d0d7
LLihao <h_91@me.com>
2017-02-02 22:36:00 -0800

    2.3 Book Solution

--- src/Chapter02.java
+++ src/Chapter02.java
@@ -151,6 +151,19 @@ class KToLast {
  *
  */
 
+class  DeleteMiddleNode {
+    public boolean deleteMiddle(Node n) {
+        if (n == null || n.next == null) {
+            return false;
+        }
+        n.data = n.next.data;
+        n.next = n.next.next;
+        return true;
+    }
+
+}
+
+
 
 /**
  *
