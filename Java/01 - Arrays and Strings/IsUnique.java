7971bfcf0ed0361654de5059f9feca94d058666d
LLihao <h_91@me.com>
2017-02-04 21:57:08 -0800

    1.1 Is Unique

--- Java/01 - Arrays and Strings/IsUnique.java
+++ Java/01 - Arrays and Strings/IsUnique.java
@@ -6,6 +6,35 @@
  *
  */
 class IsUnique {
+    // When the char set of String contained in ASCII
+    boolean isUnique1(String input) {
+        if (input.length() > 128) { return false; }
+
+        boolean[] isInInput = new boolean[128];
+        for (char c : input.toCharArray()) {
+            if (isInInput[c]) {
+                return false;
+            }
+            isInInput[c] = true;
+        }
+        return true;
+    }
+
+    // When the char is only lower case letters
+    boolean isUnique2(String input) {
+        if (input.length() > 26) { return false; }
+        int table = 0;
+        for (char c : input.toCharArray()) {
+            int value = c - 'a';
+            int mask = 1 << value;
+            if ((table & mask) > 0) {
+                return false;
+            }
+            table |= mask;
+        }
+        return true;
+    }
+    
 	public static void main(String[] args) {
 		
 	}
