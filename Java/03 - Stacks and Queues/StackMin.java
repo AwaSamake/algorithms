b5dee5adc755c87fe378e7250246f3b44e49568d
LLihao <h_91@me.com>
2017-02-04 21:48:48 -0800

    3.2 Stack Min

--- Java/03 - Stacks and Queues/StackMin.java
+++ Java/03 - Stacks and Queues/StackMin.java
@@ -6,7 +6,37 @@
  *
  */
 class StackMin {
-	public static void main(String[] args) {
-		
-	}
+    private Stack stack;
+    private Stack mins;
+
+    public StackMin() {
+        stack = new Stack();
+        mins = new Stack();
+        mins.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int value) {
+        stack.push(value);
+        if (value <= (int) mins.peek()) {
+            mins.push(value);
+        }
+    }
+
+    public int pop() {
+        int val = (int) stack.pop();
+        if ((int) mins.peek() == val) {
+            mins.pop();
+        }
+        return val;
+    }
+
+    public int min() {
+        return (int) mins.peek();
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        
+    }
 }
