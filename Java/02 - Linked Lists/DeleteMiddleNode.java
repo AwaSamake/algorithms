ad9d7af008ba8e167a38b83c4bbad6ff238aedc7
LLihao <h_91@me.com>
2017-02-04 21:56:13 -0800

    2.3 Delete Middle Node

--- Java/02 - Linked Lists/DeleteMiddleNode.java
+++ Java/02 - Linked Lists/DeleteMiddleNode.java
@@ -10,6 +10,15 @@
  *
  */
 class DeleteMiddleNode {
+    public boolean deleteMiddle(Node n) {
+        if (n == null || n.next == null) {
+            return false;
+        }
+        n.data = n.next.data;
+        n.next = n.next.next;
+        return true;
+    }
+    
 	public static void main(String[] args) {
 		
 	}
