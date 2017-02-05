87aa6cdd86498bdf89afa07210dc30d36f3885e9
LLihao <h_91@me.com>
2017-02-04 21:38:42 -0800

    5.3 Flip Bit to Win

--- Java/05 - Bit Manipulation/FlipBitToWin.java
+++ Java/05 - Bit Manipulation/FlipBitToWin.java
@@ -6,6 +6,33 @@
  *
  */
 class FlipBitToWin {
+    public int flipBit(int a) {
+        if (~a == 0) {
+            // if all 1s, no need to flip
+            // return size of integer in bit
+            return Integer.BYTES * 8;
+        }
+        int previousLength = 0;
+        int currentLength = 0;
+        int maxLength = 0;
+        boolean previousBitZero = false;
+
+        while (a != 0) {
+            if ((a & 1) == 1) {
+                ++currentLength;
+                previousBitZero = false;
+            } else if ((a & 1) == 0) {
+                previousLength = (previousBitZero) ? 0 : currentLength;
+                currentLength = 0;
+                previousBitZero = true;
+            }
+            // length = previous + current + flip
+            maxLength = Math.max(maxLength, previousLength + currentLength + 1);
+            a >>>= 1;
+        }
+        return maxLength;
+    }
+    
 	public static void main(String[] args) {
 		
 	}
