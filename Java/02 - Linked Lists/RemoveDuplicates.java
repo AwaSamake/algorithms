34f4729a1c01a9b5e9ffb8299cac05bcf9a86b43
LLihao <h_91@me.com>
2017-02-04 20:33:57 -0800

    2.1 - Remove Duplicates

--- Java/02 - Linked Lists/RemoveDuplicates.java
+++ Java/02 - Linked Lists/RemoveDuplicates.java
@@ -7,7 +7,63 @@
  *
  */
 class RemoveDuplicates {
+/**
+     * Method #1:
+     */
+    public static Node removeDuplicate1(Node head) {
+        Node n = head;
+        HashSet<Integer> elements = new HashSet<>();
+        elements.add(head.data);
+        while (n != null && n.next != null) {
+            if (elements.contains(n.next.data)) {
+                n.next = n.next.next;
+            } else {
+                elements.add(n.next.data);
+                n = n.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * Method #2:
+     */
+    public static Node removeDuplicate2(Node head) {
+        Node current = head;
+        Node runnner = current;
+        while (current.next != null) {
+            while (runnner.next != null) {
+                if (current.data == runnner.next.data) {
+                    runnner.next = runnner.next.next;
+                } else {
+                    runnner = runnner.next;
+                }
+            }
+            current = current.next;
+            runnner = current;
+        }
+        return head;
+    }
+    
+    /**
+     * Testing remove duplicate methods
+     */
     public static void main(String[] args) {
-        
+        Node head = new Node(1);
+        head.appendTail(head, 2);
+        head.appendTail(head, 2);
+        head.appendTail(head, 3);
+        head.appendTail(head, 4);
+        head.appendTail(head, 5);
+        head.appendTail(head, 5);
+        head.appendTail(head, 1);
+        head.appendTail(head, 7);
+        head.appendTail(head, 7);
+                
+        head.printList(head);
+        head = removeDuplicate1(head);
+        head.printList(head);
+        head = removeDuplicate2(head);
+        head.printList(head);
     }
 }
