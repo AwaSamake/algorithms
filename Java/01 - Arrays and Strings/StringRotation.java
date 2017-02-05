dbcf78ddf69932f1c7f320230bb8cea8b4c6f7a8
LLihao <h_91@me.com>
2017-02-04 22:04:56 -0800

    1.9 String Rotation

--- Java/01 - Arrays and Strings/StringRotation.java
+++ Java/01 - Arrays and Strings/StringRotation.java
@@ -8,6 +8,12 @@
  *
  */
 class StringRotation {
+    boolean isSubstring(String s1, String s2) {
+        if (s1.length() < 1 || s1.length() != s2.length()) { return false; }
+        String s1s1 = s1 + s1;
+        return s1s1.contains(s2);
+    }
+    
 	public static void main(String[] args) {
 		
 	}
