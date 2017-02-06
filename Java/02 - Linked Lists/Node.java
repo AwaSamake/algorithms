71b9d3ca096d16262dd5d8800835f634bd8ccce1
LLihao <h_91@me.com>
2017-02-05 17:14:34 -0800

    2.0 Node

--- Java/02 - Linked Lists/Node.java
+++ Java/02 - Linked Lists/Node.java
@@ -12,16 +12,20 @@ public class Node {
         data = d;
     }
 
-    public Node appendTail(Node head, int d) {
+    public static Node appendTail(Node head, int d) {
         Node newNode = new Node(d);
+        return appendTail(head, newNode);
+    }
+    
+    public static Node appendTail(Node head, Node node) {
         if (head == null) {
-            return newNode;
+            return node;
         }
         Node n = head;
         while (n.next != null) {
             n = n.next;
         }
-        n.next = newNode;
+        n.next = node;
         return head;
     }
 
