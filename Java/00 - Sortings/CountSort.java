50332c5d83465e430a3c450ec445f80c33da219e
LLihao <h_91@me.com>
2017-02-12 14:40:15 -0800

    0.6 Count Sort

--- Java/00 - Sortings/CountSort.java
+++ Java/00 - Sortings/CountSort.java
@@ -1,25 +1,44 @@
 /**
  *
  * 0.6 Count Sort
- *     Time: O()
- *     Space: O()
+ *     Time: O(N)
+ *     Space: O(K)
  *     Runtime: ms
  *
  */
+import java.util.*;
+
+
 class CountSort {
-    public static void sort(int numbers) {
+    public static void sort(char[] characters) {
+        int[] count = new int[26];
+        for (char c : characters) {
+            int index = charToIndex(c);
+            if (index != -1) {
+                count[index]++;
+            }
+        }
         
+        int countIndex = 0, arrIndex = 0;
+        while (countIndex < 26) {
+            if (count[countIndex] > 0) {
+                characters[arrIndex++] = (char) (97 + countIndex);
+                count[countIndex]--;
+            }
+            if (count[countIndex] == 0) {
+                countIndex++;
+            }
+        }
+    }
+    
+    public static int charToIndex(char c) {
+        int index = Character.toLowerCase(c) - 'a';
+        return (index > -1 && index < 26) ? index : -1;
     }
 
     public static void main(String[] args) {
-        final int SIZE = 200000;
-        int[] numbers = new int[SIZE];
-        Random rand = new Random();
-        
-        for (int i = 0; i < SIZE; ++i) {
-            numbers[i] = Math.abs(rand.nextInt() % 10000);
-        }
-        
+        char[] numbers = "theat".toCharArray();
         sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
