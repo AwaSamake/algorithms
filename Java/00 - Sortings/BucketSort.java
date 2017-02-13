e8922a6a3b8b329acda1298ef6c1f47fb0c11898
LLihao <h_91@me.com>
2017-02-12 16:22:04 -0800

    0.8 Bucket Sort

--- Java/00 - Sortings/BucketSort.java
+++ Java/00 - Sortings/BucketSort.java
@@ -3,13 +3,33 @@
  * 0.7 Bucket Sort
  *     Time: O()
  *     Space: O()
- *     Runtime: ms
+ *     Runtime: 450ms
  *
  */
+import java.util.*;
 
 class BucketSort {
-    public static void sort(int numbers) {
-        
+    public static void sort(int[] numbers) {
+        int mask = 1;
+        LinkedList<Integer> zeroQueue = new LinkedList<Integer>();
+        LinkedList<Integer> oneQueue = new LinkedList<Integer>();
+        for (int i = Integer.BYTES * 4 - 1; i > -1; --i) {
+            for (int num : numbers) {
+                if ((num & mask) == 0) {
+                    zeroQueue.add(num);
+                } else {
+                    oneQueue.add(num);
+                }
+            }
+            int index = 0;
+            while (!zeroQueue.isEmpty()) {
+                numbers[index++] = zeroQueue.removeFirst();
+            }
+            while (!oneQueue.isEmpty()) {
+                numbers[index++] = oneQueue.removeFirst();
+            }
+            mask = mask << 1;
+        }
     }
 
     public static void main(String[] args) {
@@ -22,5 +42,6 @@ class BucketSort {
         }
         
         sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
