0403e61df37e231c9e6f562c16ba9ac02b82f4ec
LLihao <h_91@me.com>
2017-02-12 16:02:30 -0800

    0.7 Radix Sort

--- Java/00 - Sortings/RadixSort.java
+++ Java/00 - Sortings/RadixSort.java
@@ -1,14 +1,30 @@
 /**
  *
  * 0.7 Radix Sort
- *     Time: O()
- *     Space: O()
- *     Runtime: ms
+ *     Time: O(N)
+ *     Space: O(N+K)
+ *     Runtime: 172ms
  *
  */
+import java.util.*;
+
 class RadixSort {
-	public static void sort(int numbers) {
-		
+	public static int MAX_VALUE = 100000;
+	
+	public static void sort(int[] numbers) {
+		int[] count = new int[MAX_VALUE];
+		for (int num : numbers) {
+			count[num]++;
+		}
+		for (int i = 1; i < count.length; ++i) {
+			count[i] += count[i-1];
+		}
+		int[] dup = numbers.clone();
+		for (int i = 0; i < dup.length; ++i) {
+			int val = dup[i];
+			numbers[count[val]-1] = val;
+			count[val]--;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -17,9 +33,10 @@ class RadixSort {
 		Random rand = new Random();
 		
 		for (int i = 0; i < SIZE; ++i) {
-		    numbers[i] = Math.abs(rand.nextInt() % 10000);
+		    numbers[i] = rand.nextInt(MAX_VALUE);
 		}
 		
 		sort(numbers);
+		System.out.println(Arrays.toString(numbers));
 	}
 }
