dd952b93fd2f8639271573bbecff2fedfb5e0a16
LLihao <h_91@me.com>
2017-02-13 20:16:22 -0800

    4.8 First Common Ancestor

--- Java/04 - Trees and Graphs/FirstCommonAncestor.java
+++ Java/04 - Trees and Graphs/FirstCommonAncestor.java
@@ -7,7 +7,125 @@
  *
  */
 class FirstCommonAncestor {
+    /**
+     * Solution #1: With Links to Parents
+     */
+    public static Node commonAncestor1(Node p, Node q) {
+        int delta = depth(p) - depth(q);
+        Node first = delta > 0 ? q : p;
+        Node second = delta > 0 ? p : q;
+        second = goUpBy(second, Math.abs(delta));
+        
+        while (first != second && first != null && second != null) {
+            first = first.parent;
+            second = second.parent;
+        }
+        return first == null || second == null ? null : first;
+    }
+    
+    public static Node goUpBy(Node node, int delta) {
+        while(delta > 0 && node != null) {
+            node = node.parent;
+            delta--;
+        }
+        return node;
+    }
+    
+    public static int depth(Node node) {
+        int depth = 0;
+        while (node != null) {
+            node = node.parent;
+            depth++;
+        }
+        return depth;
+    }
+    /**
+     * Solution #2: With Links to Parents
+     */
+    public static Node commonAncestor2(Node root, Node p, Node q) {
+        if (!covers(root, p) || !covers(root, q)) {
+            return null;
+        } else if (covers(p, q)) {
+            return p;
+        } else if (covers(q, p)) {
+            return q;
+        }
+        
+        Node sibling = getSibling(p);
+        Node parent = p.parent;
+        while (!cover(sibling, q)) {
+            sibling = getSibling(parent);
+            parent = parent.parent;
+        }
+        return parent;
+    }
+    
+    public static boolean covers(Node root, Node p) {
+        if (root == null) {
+            return false;
+        }
+        if (root == p) {
+            return true;
+        }
+        return cover(root.left, p) || covers(root.right, p);
+    }
+    
+    public static Node getSibling(Node node) {
+        if (node == null || node.parent == null) {
+            return null;
+        }
+        
+        Node parent = node.parent;
+        return parent.left == node ? parent.right : parent.left;
+    }
+    /**
+     * Solution #3: Without Links to Parents
+     */
+    public static Node commonAncestor3(Node root, Node p, Node q) {
+        if (!covers(root, p) || !covers(root, q)) {
+            return null;
+        }
+        return ancestorHelper(root, p, q);
+    }
+    
+    Node ancestorHelper(Node root, Node p, Node q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+    }
+    
 	public static void main(String[] args) {
 		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
