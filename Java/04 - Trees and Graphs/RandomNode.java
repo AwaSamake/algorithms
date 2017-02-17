f7cb1eb1168d3194eaa111c7bc8522a5685cf749
LLihao <h_91@me.com>
2017-02-16 22:15:49 -0800

    4.11 Random Node

--- Java/04 - Trees and Graphs/RandomNode.java
+++ Java/04 - Trees and Graphs/RandomNode.java
@@ -77,41 +77,87 @@ class Node {
     }
 }
 
+/**
+ *
+ * Solution #2
+ *
+ */
+class Tree {
+    Node root = null;
+    
+    public int size() {
+        return root == null ? 0 : root.size();
+    }
+    
+    public Node getRandomNode() {
+        if (root == null) {
+            return null;
+        }
+        
+        Random random = new Random();
+        int i = random.nextInt(size());
+        return root.getIthNode(i);
+    }
+    
+    punlic void insertInOrder(int value) {
+        if (root == null) {
+            root = new Node(value)
+        } else {
+            root.insertInOrder(value);
+        }
+    }
+}
+
+class Node {
+    public Node getIthNode(int i) {
+        int leftSize = left == null ? 0 : left.size();
+        if (i < leftSize) {
+            return left.getIthNode(i);
+        } else if (i == leftSize) {
+            return this;
+        } else {
+            return right.getIthNode(i - (leftSize + 1));
+        }
+    }
+    
+    public void insertInOrder(int d) {
+        if (d <= data) {
+            if (left == null) {
+                left = new Node(d);
+            } else {
+                left.insertInOrder(d);
+            }
+        } else {
+            if (right == null) {
+                right = new Node(d);
+            } else {
+                right.insertInOrder(d);
+            }
+        }
+        size++;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public int data() {
+        return data;
+    }
+    
+    public Node find(int d) {
+        if (d == data) {
+            return this;
+        } else if (d < data) {
+            return left != null ? left.find(d) : null;
+        } else {
+            return right != null ? right.find(d) : null;
+        }
+    }
+}
 
 class RandomNode {
 	public static void main(String[] args) {
 		
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
