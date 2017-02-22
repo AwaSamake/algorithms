1e23b4d5b9de94f8737b10990907ab2605bffd3b
LLihao <h_91@me.com>
2017-02-21 22:48:13 -0800

    8.1 Triple Step

--- Java/08 - Recursion and Dynamic Programming/TripleStep.java
+++ Java/08 - Recursion and Dynamic Programming/TripleStep.java
@@ -6,11 +6,15 @@
  *     possible ways the child can run up the stairs.
  *
  */
+import java.util.*;
+
 class TripleStep {
 	/**
 	 * Recursion, Brute Force
 	 */
-	public static int waysToGo(int steps) {
+	static int counter1 = 0;
+	public static int countWays(int steps) {
+		counter1++;
 		if (steps < 0) {
 			return 0;
 		}
@@ -18,11 +22,36 @@ class TripleStep {
 			return 1;
 		}
 
-		return waysToGo(steps - 1) + waysToGo(steps - 2) + waysToGo(steps - 3);
+		return countWays(steps - 1) + countWays(steps - 2) + countWays(steps - 3);
 	}
 	
+	/**
+	 * Memoization
+	 */
+	static int counter2 = 0;
+	public static int countWays(int steps, int[] counts) {
+		counter2++;
+		if (steps < 0) {
+			return 0;
+		} else if (steps == 0) {
+			return 1;
+		} else if (counts[steps] > -1) {
+			return counts[steps];
+		} else {
+			counts[steps] = countWays(steps - 1, counts) 
+							+ countWays(steps - 2, counts) 
+							+ countWays(steps - 3, counts);
+			return counts[steps];
+		}
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(waysToGo(6));
+		System.out.println(countWays(6));
+		System.out.println(counter1);
+		
+		int[] counts = new int[7];
+		Arrays.fill(counts, -1);
+		System.out.println(countWays(6, counts));
+		System.out.println(counter2);
 	}
 }
