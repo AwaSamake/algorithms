b31c88b540b3640d6c59a766dfa70a9df2297f22
LLihao <h_91@me.com>
2017-03-08 21:30:37 -0800

    Skip: 16.7 Number Max

---
+++ Java/16 - Moderate/NumberMax.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * 16.7 Number Max
+ *     Write a method that finds the maximum of two numbers. You should not use if else 
+ *     or any other comparison operator.
+ *
+ */
+
+class NumberMax {
+	public static int getMax(int num1, int num2) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
