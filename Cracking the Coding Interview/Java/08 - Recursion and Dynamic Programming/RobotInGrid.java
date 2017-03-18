cfddf75c0168ac41a501cb685095e1ff1bad8f06
LLihao <h_91@me.com>
2017-03-18 16:44:30 -0700

    update directory

---
+++ Cracking the Coding Interview/Java/08 - Recursion and Dynamic Programming/RobotInGrid.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * 8.2 Robot in a Grid
+ *     Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+ *     The robot can only move in two directions, right and down, but curtain cells are 
+ *     "off limits" such that the robot cannot steps on them. Design an algorithm to find
+ *     a path for the robot from the top left to the bottom right.
+ *
+ */
+import java.util.*;
+
+class RobotInGrid {
+	/**
+	 * Recursion, Brute Force
+	 */
+	public static int countWays() {
+		
+	}
+	
+	
+	/**
+	 * Memoization 
+	 */
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
