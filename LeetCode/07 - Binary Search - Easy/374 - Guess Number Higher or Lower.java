c947ba7884efa4e2b2f71670c1c9d375b0014989
LLihao <h_91@me.com>
2017-03-27 22:22:45 -0700

    374 - Guess Number Higher or Lower

---
+++ LeetCode/07 - Binary Search - Easy/374 - Guess Number Higher or Lower.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * 374. Guess Number Higher or Lower
+ * We are playing the Guess Game. The game is as follows:
+ *     I pick a number from 1 to n. You have to guess which number I picked.
+ *     Every time you guess wrong, I'll tell you whether the number is higher or lower.
+ *     You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1,
+ *     or 0):
+ *         -1 : My number is lower
+ *         1 : My number is higher
+ *         0 : Congrats! You got it!
+ *   Example:
+ *       n = 10, I pick 6.
+ *       Return 6.
+ *
+ */
+import java.util.*;
+
+/* The guess API is defined in the parent class GuessGame.
+	@param num, your guess
+	@return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+		int guess(int num); */
+
+public class Solution extends GuessGame {
+	public int guessNumber(int n) {
+		int lower = 1, higher = n, mid;
+		while (lower <= higher) {
+			mid = lower + (higher - lower) / 2;
+			int guess = guess(mid);
+			if (guess == 0) {
+				return mid;
+			} else if (guess == -1) {
+				higher = mid - 1;
+			} else {
+				lower = mid + 1;
+			}
+		}
+		return -1;
+	}
+}
