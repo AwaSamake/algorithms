c5f76ec489c36e2f58bad925f58fa2d9e134e531
LLihao <h_91@me.com>
2017-03-30 21:51:10 -0700

    101 - Symmetric Tree

--- LeetCode/101 - Symmetric Tree.java
+++ LeetCode/101 - Symmetric Tree.java
@@ -32,6 +32,17 @@ import java.util.*;
  */
 public class Solution {
 	public boolean isSymmetric(TreeNode root) {
+		return root == null || isSymmetric(root.left, root.right);
+	}
+	
+	public boolean isSymmetric(TreeNode left, TreeNode right) {
+		if (left == null || right == null) {
+			return left == right;
+		}
+		if (left.val != right.val) {
+			return false;
+		}
 		
+		return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
 	}
 }
