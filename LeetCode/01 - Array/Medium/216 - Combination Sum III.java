1463ee55a44a1579f67991850b078c144fa16a89
LLihao <h_91@me.com>
2017-04-16 00:13:28 -0700

    216 - Combination Sum III.java

--- LeetCode/01 - Array/Medium/216 - Combination Sum III.java
+++ LeetCode/01 - Array/Medium/216 - Combination Sum III.java
@@ -26,7 +26,7 @@
  */
 import java.util.*;
 
-class Solution {
+class Solution1 {
 	public List<List<Integer>> combinationSum3(int k, int n) {
 		List<List<Integer>> combinations = new ArrayList<List<Integer>>();
 		combinationSum3(combinations, new ArrayList<Integer>(), k, n, 0, 1);
@@ -47,3 +47,44 @@ class Solution {
 		}
 	}
 }
+
+class Solution {
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> combinations = new ArrayList<List<Integer>>();
+		boolean[] used = new boolean[10];
+		combinationSum3(combinations, new ArrayList<Integer>(), k, n, 1, used);
+		return combinations;
+	}
+	
+	public void combinationSum3(List<List<Integer>> combinations, List<Integer> combination, int k, int n, int begin, boolean[] used) {
+		if (k == 1 && n > 0 && n < 10 && n > combination.get(combination.size() - 1) && !used[n]) {
+			combination.add(n);
+			combinations.add(combination);
+			return;
+		}
+		if (k <= 0) {
+			return;
+		}
+		for (int i = begin; i < 10; ++i) {
+			if (i > n) {
+				return;
+			}
+			List<Integer> newCombination = new ArrayList<Integer>(combination);
+			newCombination.add(i);
+			used[i] = true;
+			combinationSum3(combinations, newCombination, k - 1, n - i, i + 1, used);
+			used[i] = false;
+		}
+	}
+}
+
+class test {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.combinationSum3(3, 15));
+	}
+}
+
+
+
+
