9cbd5469ee7488553243cfbf37f48c43f5ecf2c4
LLihao <h_91@me.com>
2017-04-15 23:54:55 -0700

    216 - Combination Sum III.java

---
+++ LeetCode/01 - Array/Medium/216 - Combination Sum III.java
@@ -0,0 +1,49 @@
+/**
+ *
+ * 216. Combination Sum III
+ *    Find all possible combinations of k numbers that add up to a number n, given that 
+ *    only numbers from 1 to 9 can be used and each combination should be a unique set of 
+ *    numbers.
+ *    
+ *    
+ *    Example 1:
+ *    
+ *    Input: k = 3, n = 7
+ *    
+ *    Output:
+ *    
+ *    [[1,2,4]]
+ *    
+ *    Example 2:
+ *    
+ *    Input: k = 3, n = 9
+ *    
+ *    Output:
+ *    
+ *    [[1,2,6], [1,3,5], [2,3,4]]
+ *     
+ *
+ */
+import java.util.*;
+
+class Solution {
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> combinations = new ArrayList<List<Integer>>();
+		combinationSum3(combinations, new ArrayList<Integer>(), k, n, 0, 1);
+		return combinations;
+	}
+	
+	public void combinationSum3(List<List<Integer>> combinations, List<Integer> combination, int k, int n, int sum, int begin) {
+		if (k == 0 && sum == n) {
+			combinations.add(combination);
+		}
+		if (k <= 0) {
+			return;
+		}
+		for (int i = begin; i < 10; ++i) {
+			List<Integer> newCombination = new ArrayList<Integer>(combination);
+			newCombination.add(i);
+			combinationSum3(combinations, newCombination, k - 1, n, sum + i, i + 1);
+		}
+	}
+}
