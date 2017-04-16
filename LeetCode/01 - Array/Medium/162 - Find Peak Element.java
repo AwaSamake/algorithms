b8c37d0b9eae80884fa4d8c3223d1567d7e4db62
LLihao <h_91@me.com>
2017-04-15 22:50:52 -0700

    162 - Find Peak Element.java

---
+++ LeetCode/01 - Array/Medium/162 - Find Peak Element.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * 162. Find Peak Element
+ *     A peak element is an element that is greater than its neighbors.
+ *     
+ *     Given an input array where num[i] ≠ num[i+1], find a peak element and return its 
+ *     index.
+ *     
+ *     The array may contain multiple peaks, in that case return the index to any one of 
+ *     the peaks is fine.
+ *     
+ *     You may imagine that num[-1] = num[n] = -∞.
+ *     
+ *     For example, in array [1, 2, 3, 1], 3 is a peak element and your function should 
+ *     return the index number 2.
+ *     
+ *     Note:
+ *     Your solution should be in logarithmic complexity.
+ *     
+ *
+ */
+import java.util.*;
+
+public class Solution {
+	public int findPeakElement(int[] nums) {
+		return findMin(nums, 0, nums.length);
+	}
+	
+	public int findPeakElement(int[] nums, int left, int right) {
+		if (left == right) {
+			return left;
+		}
+		int mid = (left + right) / 2;
+		if (nums[mid] < nums[mid + 1]) {
+			return findPeakElement(nums, mid + 1, right);
+		} else {
+			return findPeakElement(nums, left, mid - 1);
+		}
+	}
+}
