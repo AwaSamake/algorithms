4c7a512680da6b813bce1f06fd83c07d8bff98e1
LLihao <h_91@me.com>
2017-04-20 21:23:32 -0700

    438 - Find All Anagrams in a String.java

--- LeetCode/02 - HashMap/Medium/438 - Find All Anagrams in a String.java
+++ LeetCode/02 - HashMap/Medium/438 - Find All Anagrams in a String.java
@@ -72,13 +72,3 @@ class Solution {
 		return result;
 	}
 }
-
-class test {
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		
-		System.out.println(s.findAnagrams("cbaebabacd", "abc"));
-		
-	}
-}
-
