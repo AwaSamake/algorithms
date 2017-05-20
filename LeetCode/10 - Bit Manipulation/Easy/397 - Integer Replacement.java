df36a4c852d3955328d150a0965f9f4d3e27b099
LLihao <h_91@me.com>
2017-05-20 16:31:07 -0700

    397 - Integer Replacement.java

---
+++ LeetCode/10 - Bit Manipulation/Easy/397 - Integer Replacement.java
@@ -0,0 +1,70 @@
+/**
+ *
+ *  397. Integer Replacement
+ *    Given a positive integer n and you can do operations as follow:
+ *    
+ *    If n is even, replace n with n/2.
+ *    If n is odd, you can replace n with either n + 1 or n - 1.
+ *    What is the minimum number of replacements needed for n to become 1?
+ *    
+ *    Example 1:
+ *    
+ *    Input:
+ *    8
+ *    
+ *    Output:
+ *    3
+ *    
+ *    Explanation:
+ *    8 -> 4 -> 2 -> 1
+ *    Example 2:
+ *    
+ *    Input:
+ *    7
+ *    
+ *    Output:
+ *    4
+ *    
+ *    Explanation:
+ *    7 -> 8 -> 4 -> 2 -> 1
+ *    or
+ *    7 -> 6 -> 3 -> 2 -> 1
+ *    
+ */
+import java.util.*;
+
+public class Solution {
+	public int integerReplacement(int n) {
+		return integerReplacement(new HashMap<Integer, Integer>(), n);
+	}
+	
+	private int integerReplacement(HashMap<Integer, Integer> cache, int n) {
+		if (n == 1) {
+			return 0;
+		}
+		
+		if (cache.containsKey(n)) {
+			return cache.get(n);
+		}
+		
+		if ((n & (n - 1)) == 0) {
+			int count = 0;
+			while (n != 0) {
+				n >>>= 1;
+				count++;
+			}
+			return count - 1;
+		}
+		
+		int replacements = 0;
+		if (n % 2 == 1) {
+			replacements = Math.min(integerReplacement(cache, n + 1), 
+				integerReplacement(cache, n - 1));
+		} else {
+			replacements = integerReplacement(cache, n / 2);
+		}
+		replacements += 1;
+		cache.put(n, replacements);
+		return replacements;
+	}
+}
