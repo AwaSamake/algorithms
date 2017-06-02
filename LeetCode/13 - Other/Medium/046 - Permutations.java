3ff5c05174eaa5faeafb68227685b773b5719a72
LLihao <h_91@me.com>
2017-06-02 14:56:24 -0700

    046 - Permutations.java

--- LeetCode/13 - Other/Medium/046 - Permutations.java
+++ LeetCode/13 - Other/Medium/046 - Permutations.java
@@ -1,9 +1,8 @@
 /**
  *
- *  046. Permutations
- *    Given a collection of distinct numbers, return all possible 
- *    permutations.
- *    
+ * 046. Permutations
+ *    Given a collection of distinct numbers, return all possible permutations.
+ *
  *    For example,
  *    [1,2,3] have the following permutations:
  *    [
@@ -16,26 +15,36 @@
  *    ]
  *
  */
+
 import java.util.*;
 
-public class Solution {
-	public List<List<Integer>> permute(int[] nums) {
-		List<List<Integer>> listOfPermutations = new ArrayList<List<Integer>>();
-		permute(listOfPermutations, new ArrayList<Integer>(), nums);
-		return listOfPermutations;
-	}
-	
-	public void permute(List<List<Integer>> listOfPermutations, List<Integer> permutation, int[] nums) {
-		if (permutation.size() == nums.length) {
-			listOfPermutations.add(new ArrayList<Integer>(permutation));
-		}
-		
-		for (int num: nums) {
-			if (!permutation.contains(num)) {
-				permutation.add(num);
-				permute(listOfPermutations, permutation, nums);
-				permutation.remove(permutation.size() - 1);
-			}
-		}
-	}
+class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> permutations = new ArrayList<>();
+        List<Integer> permutation = new ArrayList<Integer>();
+        boolean[] used = new boolean[nums.length];
+        permute(nums, permutations, permutation, used);
+        return permutations;
+    }
+
+    private void permute(int[] nums, List<List<Integer>> permutations, List<Integer> permutation, boolean[] used) {
+        if (permutation.size() == nums.length) {
+            permutations.add(new ArrayList<Integer>(permutation));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (!used[i]) {
+                used[i] = true;
+                permutation.add(nums[i]);
+                permute(nums, permutations, permutation, used);
+                permutation.remove(permutation.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
 }
